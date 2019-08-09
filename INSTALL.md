213eafe63845448c5c0b8df1c1a577c57343f670
Olli-Pekka Wallin <opwallin@gmail.com>
2019-08-09 19:43:15 +0300

    Add install.md file

---
+++ INSTALL.md
@@ -0,0 +1,5 @@
+** Docker 
+
+ docker run -d -P --name rethink1 rethinkdb
+
+ docker run -p 3000:8080 -d rethinkdb
--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
   database: {
     db: "quake",
     host: process.env.RDB_HOST || "localhost",
-    port: process.env.RDB_PORT || 28015
+    port: process.env.RDB_PORT || 32769
   },
 
   port: 8090
