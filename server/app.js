ede509a84a670a11a0be0e15d02124e26f48f50f
Olli-Pekka Wallin <opwallin@gmail.com>
2019-08-18 22:06:40 +0300

    Saved changes

--- server/app.js
+++ server/app.js
@@ -1,18 +1,18 @@
+const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require("cors");
 
-const config = require("./data/config");
-
-console.log (config);
-
 let indexRouter = require('./routes/index');
+// let quakesRouter = require('./routes/quakes');
 let usersRouter = require('./routes/users');
 
 let app = express();
 app.use (cors);
+
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,6 +21,23 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+// app.use('/quakes', quakesRouter);
 app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(400));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
 
 module.exports = app;
--- server/routes/quakes.js
+++ server/routes/quakes.js
@@ -3,8 +3,24 @@ const r = require("rethinkdb");
 const express = require('express');
 const router = express.Router();
 const bluebird = require("bluebird");
+const config = require("./../data/config");
+
 
 const feedUrl = "earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson";
+
+// Fetch data from the USGS earthquake feed and transform all
+// the locations into point objects. Insert the data into the
+// `quakes` table. This query is assigned to a variable so it
+// can easily be reused in two different parts of the program.
+const refresh =
+    r.table("quakes").insert(
+        r.http(feedUrl)("features").merge(function (item) {
+            return {
+                geometry: r.point(
+                    item("geometry")("coordinates")(0),
+                    item("geometry")("coordinates")(1))
+            }
+        }), {conflict: "replace"});
 
 // Use the refresh query above to automatically update the the
 // earthquake database with new data at 30 minute intervals and
@@ -13,6 +29,7 @@ setInterval(function () {
     let conn;
     r.connect(config.database).then(function (c) {
         conn = c;
+        console.log("Refresh connect");
 
         return bluebird.join(refresh.run(conn),
             r.table("quakes")
@@ -26,7 +43,7 @@ setInterval(function () {
             if (conn)
                 conn.close();
         });
-}, 30 * 1000 * 60);
+}, 10 * 1000 * 60);
 
 let conn;
 r.connect(config.database).then(function (c) {
@@ -51,3 +68,32 @@ r.connect(config.database).then(function (c) {
         if (conn)
             conn.close();
     });
+
+router.get("/", function (req, res, next) {
+    console.log ("connected");
+
+    let conn;
+    r.connect(config.database).then(function (c) {
+        conn = c;
+
+        return r.table("quakes").orderBy(
+            r.desc(r.row("properties")("mag"))).run(conn);
+    })
+        .then(function (cursor) {
+            return cursor.toArray();
+        })
+        .then(function (result) {
+            console.log(result);
+            res.json(result);
+        })
+        .error(function (err) {
+            console.log("Error handling /quakes request:", err);
+            res.status(500).json({success: false, err: err});
+        })
+        .finally(function () {
+            if (conn)
+                conn.close();
+        });
+});
+
+module.exports = router;
