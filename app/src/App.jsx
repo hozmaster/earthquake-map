46fcb185ac4901d02bc2ab858a7a8a62e104d23b
Olli-Pekka Wallin <opwallin@gmail.com>
2019-08-24 23:21:45 +0300

    Integrate leaflet and openstreetmap to frontend

---
+++ app/src/App.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import Map from './Components/Map';
+
+import './App.css';
+
+class App extends React.Component {
+    render () {
+        return (
+            <div>
+                <Map/>
+            </div>
+        );
+
+    }
+}
+
+export default App;
---
+++ app/src/Components/Map.jsx
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2019.  Olli-Pekka Wallin All rights reserved
+ */
+
+import React from 'react';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
+const style = {
+    width: '100%',
+    height: '100vh'
+};
+class Map extends React.Component {
+    componentDidMount() {
+        // create map
+        this.map = L.map('map', {
+            center: [49.8419, 24.0315],
+            zoom: 16,
+            layers: [
+                L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                    attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                }),
+            ]
+        });
+
+        // this.marker = L.marker(this.props.markerPosition).addTo(this.map);
+
+    }
+
+    render() {
+        return <div id="map" style={style}/>
+    }
+}
+
+export default Map;
